449e32d729b8a92caf0fcb300e470fb79ae09a1d
unknown <infocom.elizabeth@ctc.cu>
2022-12-19 09:29:02 -0600

    feat: add edit and delete button with modal

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -8,7 +8,7 @@
       <tr>
         <!-- loop through each value of the fields to get the table header -->
         <th  v-for="field in testFields" :key='field' @click="sortTable(field)" > 
-          {{field}} <i class="bi bi-sort-alpha-down" aria-label='Sort Icon'></i>
+          {{field}}  
          </th>
       </tr>
     </thead>
@@ -16,10 +16,47 @@
         <!-- Loop through the list get the each student data -->
         <tr v-for="item in testData" :key='item'>
         <td v-for="field in testFields" :key='field'>{{item[field]}}</td>
+      
+        <th class="trackB"> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+           <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+          </svg>
+         </th>
+
+        <th class="trackB"  @click="deleteD = true"> 
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="black" class="bi bi-trash3" viewBox="0 0 16 16">
+           <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+          </svg>
+         </th>
+        
       </tr>
+      
     </tbody>
   </table>  
 </div> 
+
+
+
+
+<transition name="fade" appear>
+    <div class="modal-overlay" 
+         v-if="deleteD" 
+          ></div>
+  </transition>
+  <transition name="pop" appear>
+    <div class="modal" style="width:30%;"
+         role="dialog" 
+         v-if="deleteD">
+         <a @click="deleteD = false" class="modal-exit">x</a>
+         <br/><br/>
+      <h2>Seguro que desea eliminar?</h2>
+      <br/><br/> 
+      <div style="display:flex; justify-content:center">
+      <button type="submit" class="button button--accept" @click="deleteD = false">Aceptar</button>
+      <button type="submit" class="button button--cancel" @click="deleteD = false">Cancelar</button>
+    </div> 
+  </div>
+  </transition>
     </div> 
   </template>
   
@@ -46,7 +83,8 @@
     ],
     testFields: [
       'ID','Especie','Categoría','Año','Mes', 'Altas', 'Bajas'
-    ]
+    ],
+    deleteD:false
     }
   },  
   }
@@ -58,13 +96,21 @@
  th, td { 
   padding: 15px !important;
   border-radius:12px;
-  border-color: lightgray !important;
- 
+  border-color: lightgray !important; 
 }
 
 th {
   background-color: #42b983 !important;
   color:white; 
+}
+
+.trackB{ 
+  width: 5%;
+}
+
+.trackB:hover{
+  background-color: #5ecc9a !important;
+  cursor: pointer;
 }
 
 
